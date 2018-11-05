83e25d26295841a55158b5a2338f006b6dbbf8ee
Jim ETEVENARD <jimetevenard@gmail.com>
2018-11-05 15:18:54 +0100

    Jing is now in provided scope

--- src/main/java/eu/els/schematronCompiler/transform/JingXSLFetcher.java
+++ src/main/java/eu/els/schematronCompiler/transform/JingXSLFetcher.java
@@ -5,20 +5,26 @@ import javax.xml.transform.stream.StreamSource;
 
 import com.thaiopensource.relaxng.util.Driver;
 
+import eu.els.schematronCompiler.SchematronCompilationException;
+
 public class JingXSLFetcher {
 	
 	private static final String SCHEMATRON_IN_JING_JAR = "com/thaiopensource/validate/schematron/resources/";
+	private static final String ERROR_MESSAGE = "Unable to load Jing Schematron implementation ";
 	
 	
 
-	public static Source getJingXsl(String name){
+	public static Source getJingXsl(String name) throws SchematronCompilationException{
 		String xslPath = SCHEMATRON_IN_JING_JAR + name;
-		ClassLoader cl = Driver.class.getClassLoader();
-		
-		Source xsl = new StreamSource(cl.getResourceAsStream(xslPath));
-		xsl.setSystemId(cl.getResource(xslPath).toString());
-		
-		return xsl;
+		try {
+			ClassLoader cl = Driver.class.getClassLoader();		
+			Source xsl = new StreamSource(cl.getResourceAsStream(xslPath));
+			xsl.setSystemId(cl.getResource(xslPath).toString());
+			
+			return xsl;
+		} catch (Exception e){
+			throw new SchematronCompilationException(ERROR_MESSAGE + xslPath, e);
+		}
 	}
 
 	
